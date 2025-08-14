29134e37cab7c14024b7d40abd46dcf1db7c74cf
Glen Ochieng <glenochieng045@gmail.com>
2025-08-14 14:04:03 +0300

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Enemy Plane":return img`
+....ffffff.........ccc..
+....ffaaccf.......cc4f..
+.....ffccccfff...cc44f..
+....cca444aaaacccc44af..
+...c9b44aaaaaaaacc4aaf..
+..c999baaaaaaaaaaaaafc..
+.cab99111baaaaaaaaacaac.
+caaab11199aaaacccccccaaf
+faaaaaaaaaaaacaaaccfffff
+.faaaaaaaaaa44aaaaf.....
+..ffaaaaaaacf44aaaaf....
+....ffffffffff44aaaac...
+.........facfffcaaaac...
+.........fccaffffffff...
+..........fcaffff.......
+...........fffff........
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
