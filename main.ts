aae731e6a420887fe2bbb1f42f72d3b145229055
Glen Ochieng <glenochieng045@gmail.com>
2025-08-17 18:37:26 +0300

    make enemies appear from the right side (correctly now)

--- main.ts
+++ main.ts
@@ -6,9 +6,10 @@ scene.setBackgroundImage(assets.image`Empty Space`)
 game.splash("Space Shooter", "Press A to start")
 let playerPlane = sprites.create(assets.image`Player Plane`, SpriteKind.Player)
 playerPlane.setPosition(20, 58)
-controller.moveSprite(playerPlane, 0, 70)
 forever(function () {
-    enemyPlane = sprites.createProjectileFromSide(assets.image`Enemy Plane`, -50, 0)
+    enemyPlane = sprites.createProjectileFromSide(assets.image`Enemy Plane`, -100, 0)
     enemyPlane.y = randint(5, 115)
+    // This allows for the plane to be destroyed automatically after it leaves the screen
+    enemyPlane.setFlag(SpriteFlag.AutoDestroy, true)
     pause(1000)
 })
