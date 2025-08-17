a5c72bc83f41c48d1c4825639e67689cbba309eb
Glen Ochieng <glenochieng045@gmail.com>
2025-08-17 18:07:56 +0300

    add sprite and position it to the left of the screen

--- main.ts
+++ main.ts
@@ -3,3 +3,5 @@ namespace SpriteKind {
 }
 scene.setBackgroundImage(assets.image`Empty Space`)
 game.splash("Space Shooter", "Press A to start")
+let playerPlane = sprites.create(assets.image`Player Plane`, SpriteKind.Player)
+playerPlane.setPosition(20, 58)
