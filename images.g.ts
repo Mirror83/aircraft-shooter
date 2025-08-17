b60fb89ccdd229816e199cf00d86f3840ab0e3cc
Glen Ochieng <glenochieng045@gmail.com>
2025-08-17 18:23:51 +0300

    make enemy planes appear from right side

--- images.g.ts
+++ images.g.ts
@@ -3,25 +3,6 @@ namespace myImages {
 
     helpers._registerFactory("image", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "image1":
-            case "Enemy Plane":return img`
-....ffffff.........ccc..
-....ffaaccf.......cc4f..
-.....ffccccfff...cc44f..
-....cca444aaaacccc44af..
-...c9b44aaaaaaaacc4aaf..
-..c999baaaaaaaaaaaaafc..
-.cab99111baaaaaaaaacaac.
-caaab11199aaaacccccccaaf
-faaaaaaaaaaaacaaaccfffff
-.faaaaaaaaaa44aaaaf.....
-..ffaaaaaaacf44aaaaf....
-....ffffffffff44aaaac...
-.........facfffcaaaac...
-.........fccaffffffff...
-..........fcaffff.......
-...........fffff........
-`;
             case "image2":
             case "Empty Space":return img`
 ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
@@ -182,6 +163,25 @@ ffffffc222c22222222222f.
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
+`;
+            case "image1":
+            case "Enemy Plane":return img`
+....ffffff.........ccc..
+....ffaaccf.......cc4f..
+.....ffccccfff...cc44f..
+....cca444aaaacccc44af..
+...c9b44aaaaaaaacc4aaf..
+..c999baaaaaaaaaaaaafc..
+.cab99111baaaaaaaaacaac.
+caaab11199aaaacccccccaaf
+faaaaaaaaaaaacaaaccfffff
+.faaaaaaaaaa44aaaaf.....
+..ffaaaaaaacf44aaaaf....
+....ffffffffff44aaaac...
+.........facfffcaaaac...
+.........fccaffffffff...
+..........fcaffff.......
+...........fffff........
 `;
         }
         return null;
--- main.ts
+++ main.ts
@@ -1,7 +1,14 @@
 namespace SpriteKind {
     export const PlayerLaser = SpriteKind.create()
 }
+let enemyPlane: Sprite = null
 scene.setBackgroundImage(assets.image`Empty Space`)
 game.splash("Space Shooter", "Press A to start")
 let playerPlane = sprites.create(assets.image`Player Plane`, SpriteKind.Player)
 playerPlane.setPosition(20, 58)
+controller.moveSprite(playerPlane, 0, 70)
+forever(function () {
+    enemyPlane = sprites.createProjectileFromSide(assets.image`Enemy Plane`, -50, 0)
+    enemyPlane.y = randint(5, 115)
+    pause(1000)
+})
